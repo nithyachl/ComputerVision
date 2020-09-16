7a1443d791c5087e397e4352f16ea2d7311a5af9
nithyachl <33202100+nithyachl@users.noreply.github.com>
2020-09-15 22:36:06 -0400

    //vs 2

--- l01.cpp
+++ l01.cpp
@@ -4,53 +4,34 @@
 
 # include <iostream>
 # include <fstream>
-
+#include <cstdio>
 
 using namespace std;
 
 
-typedef int matrix[800][800];
+
 
 int main() {
 
-    ofstream MyFile("first.ppm", ios_base::out | ios_base::binary);
-    MyFile << "P3" << endl << 800u << ' ' << 800u << endl << "256" << endl;
+    constexpr auto dimx = 800u, dimy = 800u;
 
+    using namespace std;
+    ofstream ofs("one.ppm", ios_base::out| ios_base::binary );
+    ofs << "P6" << endl << dimx << ' ' << dimy << endl << "255" << endl;
 
+    for (auto j = 0u; j < dimy; ++j) {
+        for (auto i = 0u; i < dimx; ++i) {
+            ofs << (char)(255) <<(char) (255) << (char) (255) ;
 
-    for (auto i = 0u; i < 800u; ++i)
-    {
-        for (auto j = 0u; j < 800u; ++j)
-        {
-            MyFile << 1 <<1 << 1;
         }
 
     }
-
-
-
-    // 10 test cases
-
-
-    MyFile.close();
-
-    return 0;
-/**
-
-    constexpr auto dimx = 800, dimy = 800;
-
-    using namespace std;
-    ofstream ofs("first.ppm", ios_base::out | ios_base::binary);
-    ofs << "P3" << endl << dimx << ' ' << dimy << endl << "256" << endl;
-
-    for (auto j = 0u; j < dimy; ++j)
-        for (auto i = 0u; i < dimx; ++i)
-            ofs << 1 << 1<< 1;   // red, green, blue
-
     ofs.close();
 
     return EXIT_SUCCESS;
-**/
+
+
+
 }
 
 
